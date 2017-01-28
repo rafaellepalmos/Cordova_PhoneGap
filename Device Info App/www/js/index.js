2340f70f7cdd5a5217eea6a2b89db73e52f7c184
rafaellepalmos <rafaellepalmos@gmail.com>
2017-01-28 23:37:27 +0700

    A work made with PhoneGap

---
+++ Device Info App/www/js/index.js
@@ -0,0 +1,63 @@
+/*
+ * Licensed to the Apache Software Foundation (ASF) under one
+ * or more contributor license agreements.  See the NOTICE file
+ * distributed with this work for additional information
+ * regarding copyright ownership.  The ASF licenses this file
+ * to you under the Apache License, Version 2.0 (the
+ * "License"); you may not use this file except in compliance
+ * with the License.  You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing,
+ * software distributed under the License is distributed on an
+ * "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY
+ * KIND, either express or implied.  See the License for the
+ * specific language governing permissions and limitations
+ * under the License.
+ */
+var app = {
+    // Application Constructor
+    initialize: function() {
+        this.bindEvents();
+    },
+    // Bind Event Listeners
+    //
+    // Bind any events that are required on startup. Common events are:
+    // 'load', 'deviceready', 'offline', and 'online'.
+    bindEvents: function() {
+        document.addEventListener('deviceready', this.onDeviceReady, false);
+    },
+    // deviceready Event Handler
+    //
+    // The scope of 'this' is the event. In order to call the 'receivedEvent'
+    // function, we must explicitly call 'app.receivedEvent(...);'
+    onDeviceReady: function() {
+        //app.receivedEvent('deviceready');
+		var cordova = document.getElementById("device-cordova");
+		cordova.textContent = device.cordova;
+		var model = document.getElementById("device-model");
+		model.textContent = device.model; //set some value into our li
+		var platform = document.getElementById("device-platform");
+		platform.textContent = device.platform;
+		var uuid = document.getElementById("device-uuid");
+		uuid.textContent = device.uuid;
+		var version = document.getElementById("device-version");
+		version.textContent = device.version;
+		var isVirtual = document.getElementById("device-isVirtual");
+		isVirtual.textContent = device.isVirtual;
+		var serial = document.getElementById("device-serial");
+		serial.textContent = device.serial;
+    },
+    // Update DOM on a Received Event
+    receivedEvent: function(id) {
+        var parentElement = document.getElementById(id);
+        var listeningElement = parentElement.querySelector('.listening');
+        var receivedElement = parentElement.querySelector('.received');
+
+        listeningElement.setAttribute('style', 'display:none;');
+        receivedElement.setAttribute('style', 'display:block;');
+
+        console.log('Received Event: ' + id);
+    }
+};
